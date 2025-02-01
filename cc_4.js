87159d176dbf54849cf5a9bb19cb14f9449a5518
Haley Goodfield <haleygoodfield@usf.edu>
2025-02-01 08:40:51 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,18 @@
+// Task 1: If Statements
+
+let purchaseAmount= 114.99; // Declare a variable purchaseAmount with a value.
+let finalAmount = purchaseAmount // Amount after discoumt
+const discount = .25; //25% Discount
+if (purchaseAmount > 100) { // Use an if statement to apply a discount if the amount is greater than $100.
+    finalAmount = purchaseAmount * discount;
+}
+
+console.log('Final Amount: $ $${finalAmount}'); // Log the final amount to the console using a template literal, e.g.
+
+// fix template literal later... 
+
+// Task 2: For Loop
+// Declare an array sales with at least five sales figures
+
+
+
